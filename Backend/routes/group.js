dd9519d22424d042fc697a8aa015225569d4b58c
Sanskargh7 <93363422+Sanskargh7@users.noreply.github.com>
2022-08-10 11:54:24 +0530

    Update group.js

--- Backend/routes/group.js
+++ Backend/routes/group.js
@@ -5,5 +5,5 @@ const loginVerify=require('../middleware/auth')
 router.post('/creategroup',loginVerify,groupController.createGroup)
 router.get('/getgroups',loginVerify,groupController.getGroup)
 router.get('/isAdmin',loginVerify,groupController.getIsAdmin)
-//router.post('/addUsertogroup',loginVerify,groupController.addusertoGroup)
+
 module.exports=router
